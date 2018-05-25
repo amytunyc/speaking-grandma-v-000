bc39ccee672eb098d0ee2fc687f308e821f70982
Amy Tu <siamesebb@gmail.com>
2018-05-25 02:38:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if !phrase.upcase
+  if phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMKIN!"
+    elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
     return "HUH?! SPEAK UP, SONNY!"
-    el
+  end
 end 
